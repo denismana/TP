3654d812671ba4b8f7a54f5e2395fd9b59eb43c2
denismana <deni_fotbalist@yahoo.com>
2020-04-29 06:34:25 +0200

    Update 6_4.c

--- lab 6/6_4.c
+++ lab 6/6_4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define CITIRE(A,TIP,X,MIN,MAX)
+#define CITIRE(A,TIP,X,MIN,MAX)                 ///de ce imi cere inainte de define o paranteza sau ceva?
     {
         printf(A);
         scanf("%#TIP",&X);
